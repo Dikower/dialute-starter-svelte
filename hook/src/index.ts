56d1a6603a63d197206da67e01da3edd3b2ecc81
Din Dmitriy <dmitriy1d01@gmail.com>
2022-05-28 21:48:14 +0300

    🛰 Added deployment on vercel

--- hook/src/index.ts
+++ hook/src/index.ts
@@ -2,7 +2,7 @@ import { Dialute, SberRequest } from 'dialute';
 
 function* script(r: SberRequest) {
   const rsp = r.buildRsp();
-  const state = {count: 0}
+  const state = { count: 0 };
 
   rsp.msg = 'Hello world from Dialute!';
   rsp.data = state;
@@ -13,16 +13,15 @@ function* script(r: SberRequest) {
   while (true) {
     if (r.type === 'SERVER_ACTION' && r.act?.action_id === 'click') {
       state.count++;
-      rsp.msg = 'You have clicked the button'
+      rsp.msg = 'You have clicked the button';
     } else {
-      rsp.msg = 'You have said something'
+      rsp.msg = 'You have said something';
     }
     // yield 'Hello world from Dialute!';  // also possible
     yield rsp;
   }
 }
 
-Dialute
-  .fromEntrypoint(script as GeneratorFunction)
-  .shareApp('../app/public')
+Dialute.fromEntrypoint(script as GeneratorFunction)
+  .shareApp('app/public')
   .start();
